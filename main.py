4b52cf95fc382d9c0e4f1d21e69340eea859a61f
Daniel Solarte <danielsolarte1466@gmail.com>
2024-09-12 20:53:25 -0500

    abordamos la sesion 2

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI #importa la clase FastAPI de fastapi
+from fastapi.responses import HTMLResponse #importa la clase HTMLResponse de fastapi
+from movies_list import movies_list
 
 app = FastAPI() #crea una instancia de la clase FastAPI
 app.title = 'Mi primera aplicación de películas y análisis de datos'
-app.version = '1.0.1'
+app.version = '0.0.1'
 @app.get('/', tags=['Home'])
 def message(): #definiendo una ruta
-    return 'Hello World' #devuelve un diccionario
+    return HTMLResponse('<h1>Hola mundo</h1>') #devuelve un objeto HTMLResponse
 
-
+@app.get('/movies', tags=['Movies'])#definiendo una ruta de la clase fastapi
+def movies(): #definiendo una ruta
+    return movies_list
+@app.get('/movies/{id}', tags=['Movies'])#app get movies id
+def get_movie(id:int): #definiendo una ruta
+    for item in movies_list:
+        if item['id'] == id:
+            return item
+    return []
---
+++ movies_list.py
@@ -0,0 +1,68 @@
+movies_list = [
+    {
+        "id": 1,
+        "title": "Deadpool & wolverine",
+        "overview": "Deadpool and the wolverine are back",
+        "year": 2024,
+        "rating": 9.0
+    },
+    {
+        "id": 2,
+        "title": "Pulp Fiction",
+        "overview": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+        "year": 1994,
+        "rating": 9.0
+    },
+    {
+        "id": 3,
+        "title": "Terminator 2",
+        "overview": "The second part of the plot for the 1984 Terminator movie.",
+        "year": 1991,
+        "rating": 9.0
+    },
+    {
+        "id": 4,
+        "title": "Batman the dark knight",
+        "overview": "Batman the dark knight is about the life of the batman",
+        "year": 2008,
+        "rating": 9.0
+    },
+    {
+        "id": 5,
+        "title": "Scarface",
+        "overview": "Scarface is a movie about the gansters and their families",
+        "year": 1983,
+        "rating": 9.0
+    },
+    {
+        "id": 6,
+        "title": "the wolf of wall street",
+        "overview": "the wolf of wall street is about the life of the stock market",
+        "year": 2013,
+        "rating": 9.0
+    },
+    {
+        "id": 7,
+        "title": "avengers infinity war",
+        "overview": "avengers infinity war is about the life of the super heroes",
+        "year": 2018,
+        "rating": 9.0
+    },
+    {
+        "id": 8,
+        "title": "jurasic park",
+        "overview": "jurasic park is about the life of the dinosaurs",
+        "year": 1997,
+        "rating": 7.0
+    },
+    {
+        "id": 9,
+        "title": "Rocky",
+        "overview": "rocky is a movie about a boxing man and his friends",
+        "year": 1976,
+        "rating": 8.0
+    },
+    
+]
+
+   
