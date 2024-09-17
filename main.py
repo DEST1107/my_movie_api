6db702951d3e766c9a020449f4cd37f8c3fe233a
Daniel Solarte <danielsolarte1466@gmail.com>
2024-09-17 18:43:31 -0500

    se abordaron las sesiones 3 y 4

--- main.py
+++ main.py
@@ -1,20 +1,58 @@
-from fastapi import FastAPI #importa la clase FastAPI de fastapi
-from fastapi.responses import HTMLResponse #importa la clase HTMLResponse de fastapi
+from fastapi import FastAPI, Body #importa la clase FastAPI de la libreria fastapi
+from fastapi.responses import HTMLResponse #importa la clase HTMLResponse de la libreria fastapi
+
+app = FastAPI() #estamos creando una instancia de FastAPI
+app.title = "Mi aplicación con FastAPI" #asigna el valor de title a la instancia de la clase FastAPI
 from movies_list import movies_list
+app.version = "0.0.1"
 
-app = FastAPI() #crea una instancia de la clase FastAPI
-app.title = 'Mi primera aplicación de películas y análisis de datos'
-app.version = '0.0.1'
-@app.get('/', tags=['Home'])
-def message(): #definiendo una ruta
-    return HTMLResponse('<h1>Hola mundo</h1>') #devuelve un objeto HTMLResponse
 
-@app.get('/movies', tags=['Movies'])#definiendo una ruta de la clase fastapi
-def movies(): #definiendo una ruta
+
+@app.get('/', tags=["Home"]) #definimos una ruta de la clase FastAPI 
+def message(): #definimos una funcion de la ruta 
+    return HTMLResponse('<h1>Hello World</h1>') #retorna un objeto de la clase HTMLResponse
+
+@app.get('/movies', tags=["Movies"]) #definimos una ruta de la clase FastAPI
+def get_movies(): #aqui puse get_movies y estaba solo movies
     return movies_list
-@app.get('/movies/{id}', tags=['Movies'])#app get movies id
-def get_movie(id:int): #definiendo una ruta
+
+@app.get('/movies/{id}' , tags=["Movies"])
+def get_movie(id: int):
     for item in movies_list:
-        if item['id'] == id:
+        if item["id"] == id:
             return item
     return []
+
+@app.get('/movies/', tags=["Movies"])
+def get_movies_by_category(category: str, year: int):
+    return [ item for item in movies_list if item ['category'] == category]
+
+@app.post('/movies', tags=['Movies'])
+def create_movie(id: int =Body(), title: str =Body(), overview: str =Body(), year: int =Body(), rating: float =Body(), category: str =Body()):
+    movies_list.append({
+        "id": id,
+        "title": title,
+        "overview": overview,
+        "year": year,
+        "rating": rating,
+        "category": category
+    })
+    return movies_list
+
+@app.put('/movies/{id}', tags=['Movies'])
+def update_movie(id: int, title: str =Body(), overview: str =Body(), year: int =Body(), rating: float =Body(), category: str =Body()):
+    for item in movies_list:
+        if item["id"] == id:
+            item['title'] = title,
+            item['overview'] = overview,
+            item['year'] = year,
+            item['rating'] = rating,
+            item['category'] = category,
+            return movies_list
+
+@app.delete('/movies/{id}', tags=['Movies'])
+def delete_movie(id: int):
+        for item in movies_list:
+            if item["id"] == id:
+                movies_list.remove(item)
+                return movies_list
--- movies_list.py
+++ movies_list.py
@@ -4,6 +4,7 @@ movies_list = [
         "title": "Deadpool & wolverine",
         "overview": "Deadpool and the wolverine are back",
         "year": 2024,
+        "category": "action",
         "rating": 9.0
     },
     {
@@ -11,6 +12,7 @@ movies_list = [
         "title": "Pulp Fiction",
         "overview": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
         "year": 1994,
+        "category": "action",
         "rating": 9.0
     },
     {
@@ -18,6 +20,7 @@ movies_list = [
         "title": "Terminator 2",
         "overview": "The second part of the plot for the 1984 Terminator movie.",
         "year": 1991,
+        "category": "action",
         "rating": 9.0
     },
     {
@@ -25,6 +28,7 @@ movies_list = [
         "title": "Batman the dark knight",
         "overview": "Batman the dark knight is about the life of the batman",
         "year": 2008,
+        "category": "action", 
         "rating": 9.0
     },
     {
@@ -32,6 +36,7 @@ movies_list = [
         "title": "Scarface",
         "overview": "Scarface is a movie about the gansters and their families",
         "year": 1983,
+        "category": "action", 
         "rating": 9.0
     },
     {
@@ -39,6 +44,7 @@ movies_list = [
         "title": "the wolf of wall street",
         "overview": "the wolf of wall street is about the life of the stock market",
         "year": 2013,
+        "category": "drama", 
         "rating": 9.0
     },
     {
@@ -46,12 +52,14 @@ movies_list = [
         "title": "avengers infinity war",
         "overview": "avengers infinity war is about the life of the super heroes",
         "year": 2018,
+        "category": "action", 
         "rating": 9.0
     },
     {
         "id": 8,
         "title": "jurasic park",
         "overview": "jurasic park is about the life of the dinosaurs",
+        "category": "adventure", 
         "year": 1997,
         "rating": 7.0
     },
@@ -59,6 +67,7 @@ movies_list = [
         "id": 9,
         "title": "Rocky",
         "overview": "rocky is a movie about a boxing man and his friends",
+        "category": "action",
         "year": 1976,
         "rating": 8.0
     },
